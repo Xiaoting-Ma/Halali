c55c2d3c8499a1474c1aef847684b6b3d41aa23d
Xiaoting-Ma <xiaoting.ma1012@gmail.com>
2026-03-04 13:47:52 +0800

    Sitemap added

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,23 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
+import Sitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    Sitemap({
+      hostname: 'https://halali.co',
+      lastmod: new Date(),
+      changefreq: 'weekly',
+      priority: 0.7,
+      // 其他可选配置：routes 等
+    }),
   ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
-  },
+  }
 })
