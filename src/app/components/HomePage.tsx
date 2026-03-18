df74e41ddb4b1d2c63f10b8dae26033c5f5d9fea
Xiaoting-Ma <xiaoting.ma1012@gmail.com>
2026-03-18 22:29:19 +0800

    Banner video updated

--- src/app/components/HomePage.tsx
+++ src/app/components/HomePage.tsx
@@ -152,7 +152,7 @@ export function HomePage({ onNavigate }: HomePageProps) {
             >
               {/* ⬇️⬇️⬇ 在这里放你的视频链接 ⬇️⬇️⬇️ */}
               <source
-                src="/halali_website_video.mov"
+                src="/halali_website_video.mp4"
                 type="video/mp4"
               />
             </video>
